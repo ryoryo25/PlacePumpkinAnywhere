973aa24771fd4999ebcc9e77e039a2295bfb251c
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-04-29 23:41:16 +0900

    change name

--- src/main/java/ryoryo/ppa/asm/ASMHooks.java
+++ src/main/java/ryoryo/ppa/asm/ASMHooks.java
@@ -3,7 +3,7 @@ package ryoryo.ppa.asm;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class ModHooks
+public class ASMHooks
 {
 	public static boolean pumpkinHook(World world, BlockPos pos)
 	{
